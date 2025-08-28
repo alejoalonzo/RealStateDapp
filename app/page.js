b05286ca164ac988b163fd5bb64258e83df8b25d
alejandro <alejandro.12w@gmail.com>
2025-08-27 21:39:39 -0700

    WIP deploy6

--- app/page.js
+++ app/page.js
@@ -2,10 +2,14 @@ import Image from "next/image";
 
 //INTERNAL IMPORTS
 import { Home, VideoHero, PropertyPortfolio, Footer } from "@/components";
+import Header from "@/components/header/header.jsx";
 
 export default function HomePage() {
   return (
     <div className="w-full overflow-x-hidden">
+      <div className="w-full absolute top-0 left-0 right-0 z-30">
+        <Header />
+      </div>
       <Home />
       <VideoHero />
       <PropertyPortfolio />
